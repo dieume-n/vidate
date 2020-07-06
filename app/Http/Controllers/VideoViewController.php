ab21f641a150fd78137fb95e8f986bc615051f33
Luteranya Nyange <dieume.dev@gmail.com>
2020-07-06 17:20:09 +0300

    30 seconds cooldown when logging views

--- app/Http/Controllers/VideoViewController.php
+++ app/Http/Controllers/VideoViewController.php
@@ -4,12 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Video;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class VideoViewController extends Controller
 {
+    const BUFFER = 30;
+
     public function store(Request $request, Video $video)
     {
         if (!$video->canBeAccessed($request->user())) {
+            return;
+        }
+
+        // Grab last view from user
+        if ($request->user()) {
+            $lastUserView = $video->views()->latestbyUser($request->user())->first();
+
+            if ($this->withinBuffer($lastUserView)) {
+                return;
+            }
+        }
+
+        // Grab last view by Ip address
+        $latestIpView = $video->views()->latestByIp($request->ip())->first();
+
+        if ($this->withinBuffer($latestIpView)) {
             return;
         }
 
@@ -19,5 +38,10 @@ class VideoViewController extends Controller
         ]);
 
         return response()->json([], 200);
+    }
+
+    protected function withinBuffer($view)
+    {
+        return $view && $view->created_at->diffInSeconds(Carbon::now()) < self::BUFFER;
     }
 }
--- app/VideoView.php
+++ app/VideoView.php
@@ -16,4 +16,19 @@ class VideoView extends Model
     {
         return $this->belongsTo(Video::class);
     }
+
+    public function scopeByUser($query, User $user)
+    {
+        return $query->where('user_id', $user->id);
+    }
+
+    public function scopeLatestByUser($query, User $user)
+    {
+        return $query->byUser($user)->orderBy('created_at', 'desc')->take(1);
+    }
+
+    public function scopeLatestByIp($query, $ip)
+    {
+        return $query->where('ip_address', $ip)->orderBy('created_at', 'desc')->take(1);
+    }
 }
