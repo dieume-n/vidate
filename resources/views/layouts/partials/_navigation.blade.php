383460b03f8be9ae3deab549ade9c9eac6ae1e2d
Luteranya Nyange <dieume.dev@gmail.com>
2020-07-08 17:13:39 +0300

    add home route to navbar

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -36,6 +36,9 @@
                 <li class="nav-item">
                     <a class="nav-link text-uppercase font-weight-bolder" href="{{ route('upload') }}">Upload</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link text-uppercase" href="{{ route('home') }}">Home</a>
+                </li>
                 <li class="nav-item dropdown">
 
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
